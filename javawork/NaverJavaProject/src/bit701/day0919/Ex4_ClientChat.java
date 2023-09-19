31ff498d4e7b29f45c8f66886fe3d8495dd0b41a
moonhee <zipsy327@gmail.com>
2023-09-19 10:58:30 +0900

    채팅

--- javawork/NaverJavaProject/src/bit701/day0919/Ex4_ClientChat.java
+++ javawork/NaverJavaProject/src/bit701/day0919/Ex4_ClientChat.java
@@ -1,10 +1,45 @@
 package bit701.day0919;
 
-public class Ex4_ClientChat {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class Ex4_ClientChat extends JFrame{
+	String nickName;
+	JTextArea area;
+	JTextField tfNickName,tfMessage;
+	JButton btnSend,btnConnect;
+	
+	public Ex4_ClientChat() {
+		// TODO Auto-generated constructor stub
+		this.setBounds(1000, 100, 400, 400);
+		this.setDesign();
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	private void setDesign() {
+		// TODO Auto-generated method stub
+		JPanel pTop=new JPanel();
+		tfNickName=new JTextField("이문희");//자기이름으로 초기값
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Ex4_ClientChat client=new Ex4_ClientChat();
 
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
--- javawork/NaverJavaProject/src/bit701/day0919/Ex4_ServerChat.java
+++ javawork/NaverJavaProject/src/bit701/day0919/Ex4_ServerChat.java
@@ -1,9 +1,13 @@
 package bit701.day0919;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 
-public class Ex4_ServerChat extends JFrame{
+public class Ex4_ServerChat extends JFrame implements Runnable{
 	JTextArea area;
 	
 	public Ex4_ServerChat() {
@@ -15,6 +19,30 @@ public class Ex4_ServerChat extends JFrame{
 		this.setVisible(true);
 	}
 	
+	//1번 쓰레드의 run 메소드
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		//서버소켓 생성
+		ServerSocket serverSocket=null;
+		try {
+			serverSocket=new ServerSocket(6000);
+			area.append("서버 소켓 생성 성공!!\n");
+			
+			//접속하는 클라이언트들 허용
+			while(true)
+			{
+				//대기중이다가
+				//접송요청하는 클라이언트 허용,허용이 되면 Socket 이 만들어진다
+				Socket socket=serverSocket.accept();
+				System.out.println("접속허용 IP:"+socket.getInetAddress().getHostAddress()+"\n");
+			}
+			
+		} catch (IOException e) {
+			area.append("서버 소켓 생성 실패:"+e.getMessage());
+		}
+	}
+	
 	private void setDesign() {
 		// TODO Auto-generated method stub
 		area=new JTextArea();
@@ -24,6 +52,16 @@ public class Ex4_ServerChat extends JFrame{
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Ex4_ServerChat serverChat=new Ex4_ServerChat();
+		Thread th=new Thread(serverChat);
+		th.start();//1번 run 메소드 호출
 	}
-
 }
+
+
+
+
+
+
+
+
+
