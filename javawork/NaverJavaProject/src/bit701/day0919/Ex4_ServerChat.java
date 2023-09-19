ac2f2850336a15b09bfd3ce8bc10d8cb5f74c2c5
moonhee <zipsy327@gmail.com>
2023-09-19 11:43:34 +0900

    채팅

--- javawork/NaverJavaProject/src/bit701/day0919/Ex4_ServerChat.java
+++ javawork/NaverJavaProject/src/bit701/day0919/Ex4_ServerChat.java
@@ -1,10 +1,15 @@
 package bit701.day0919;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 import javax.swing.JFrame;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 public class Ex4_ServerChat extends JFrame implements Runnable{
@@ -35,7 +40,8 @@ public class Ex4_ServerChat extends JFrame implements Runnable{
 				//대기중이다가
 				//접송요청하는 클라이언트 허용,허용이 되면 Socket 이 만들어진다
 				Socket socket=serverSocket.accept();
-				System.out.println("접속허용 IP:"+socket.getInetAddress().getHostAddress()+"\n");
+				area.append("접속허용 IP:"+socket.getInetAddress().getHostAddress()+"\n");
+				this.autoScroll();
 			}
 			
 		} catch (IOException e) {
@@ -43,10 +49,46 @@ public class Ex4_ServerChat extends JFrame implements Runnable{
 		}
 	}
 	
+	//접속한 클라이언트와 대화를 주고받을 내부클래스
+	class ClientMember extends Thread
+	{
+		Socket mySocket;
+		BufferedReader br;
+		PrintWriter pw;
+		String nickName;
+		
+		public ClientMember(Socket socket) {
+			// TODO Auto-generated constructor stub
+			mySocket=socket;
+			//대화를 주고받을수 있게 io 클래스 얻기
+			InputStream is=null;
+			try {
+				is=mySocket.getInputStream();
+				br=new BufferedReader(new InputStreamReader(is));
+				pw=new PrintWriter(mySocket.getOutputStream());				
+			} catch (IOException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+			}
+			
+		}
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			super.run();
+		}
+	}
+	
+	public void autoScroll()
+	{
+		int n=area.getDocument().getLength();//총 라인수
+		area.setCaretPosition(n);//마지막줄로 위치 변경
+	}
+	
 	private void setDesign() {
 		// TODO Auto-generated method stub
 		area=new JTextArea();
-		this.add(area);
+		this.add(new JScrollPane(area));
 		
 	}
 	public static void main(String[] args) {
