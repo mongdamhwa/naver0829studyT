b22e5a28a122be921bdef73f3dfb593cd1e05b5a
moonhee <zipsy327@gmail.com>
2023-09-22 17:03:25 +0900

    jdbc

--- javawork/NaverJavaProject/src/bit701/day0922/Ex3_MysqlGroupSawon.java
+++ javawork/NaverJavaProject/src/bit701/day0922/Ex3_MysqlGroupSawon.java
@@ -1,10 +1,28 @@
 package bit701.day0922;
 
 public class Ex3_MysqlGroupSawon {
+	static final String MYSQL_DRIVER="com.mysql.cj.jdbc.Driver";
+	static final String MYSQL_URL="jdbc:mysql://localhost:3306/bit701?serverTimezone=Asia/Seoul";
 
+	Ex3_MysqlGroupSawon(){
+		try {
+			Class.forName(MYSQL_DRIVER);
+			//System.out.println("Mysql 드라이버 성공");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Mysql 드라이버 실패:"+e.getMessage());
+		}
+	}
+	
+	public void sawonBunseok()
+	{
+		String sql="select buseo,count(*) count,max(score) maxscore,"
+				+ "min(score) minscore,avg(score) avgscore from sawon group by buseo";
+		
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Ex3_MysqlGroupSawon ex=new Ex3_MysqlGroupSawon();
+		ex.sawonBunseok();
 	}
 
 }
