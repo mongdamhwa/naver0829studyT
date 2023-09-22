888f76328acf078f92dc6b04f6b509b00f32f6c9
moonhee <zipsy327@gmail.com>
2023-09-22 12:49:41 +0900

    mysql

--- dbwork/mysql_0922.sql
+++ dbwork/mysql_0922.sql
@@ -47,7 +47,40 @@ insert into sawon values (null,'손지창',99,'남자','인사부');
 insert into sawon values (null,'손호준',100,'남자','교육부');
 insert into sawon values (null,'이서진',87,'남자','인사부');
 insert into sawon values (null,'광수',74,'남자','홍보부');
+insert into sawon values (null,'이이랑',97,'남자','인사부');
+
+-- select limit 
+select * from sawon order by name;
+select * from sawon order by name limit 0,3; -- 0(첫글)번부터 3개 조회
+select * from sawon order by name limit 3,3; -- 3번부터 3개
    
+-- 문자함수들 연습
+select concat('Hello','Kitty','!!') from dual;
+-- sawon 의 name 에서 '이' 를 포함한 데이타 조회
+select * from sawon where name like concat('%','이','%');
+select replace('Happy Day!!','Happy','Nice') from dual;-- Happy 를 Nice 로 변경해서 출력
+select instr('Happy Day','Day') from dual; -- 7 : Day 의 위치찾기 (1부터)
+select instr('Happy Day','Hello') from dual; -- 없을경우 0
+
+-- 추출
+select left('Have a Nice Day',4) from dual; -- 왼쪽에서 4글자 추출 : Have
+select right('Have a Nice Day',8) from dual; -- 오른쪽에서 8글자 추출 : Nice Day
+select mid('Have a Nice Day',6,6) from dual; -- 왼쪽에서 4글자 추출 :6번부터 6글자 : a Nice
+select substring('Have a Nice Day',6,6) from dual; -- 왼쪽에서 4글자 추출 :6번부터 6글자 : a Nice
+
+-- 공백제거
+select ltrim('    Hello    ') from dual; -- 왼쪽 공백 제거
+select rtrim('    Hello    ') from dual; -- 오른쪽 공백 제거
+select trim('    Hello    ') from dual; -- 양쪽 공백 제거
+
+-- 대소문자 변환
+select lcase('Happy') from dual; -- 소문자
+select lower('Happy') from dual; -- 소문자
+select ucase('Happy') from dual; -- 대문자
+select upper('Happy') from dual; -- 대문자
+
+select reverse('Happy') from dual; -- 거꾸로
+
 
 
 
