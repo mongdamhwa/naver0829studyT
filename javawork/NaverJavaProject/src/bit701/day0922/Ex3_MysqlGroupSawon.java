548ae3fd09f68609dd68b539e191f2e629f6e42e
moonhee <zipsy327@gmail.com>
2023-09-25 10:28:16 +0900

    jdbc

--- javawork/NaverJavaProject/src/bit701/day0922/Ex3_MysqlGroupSawon.java
+++ javawork/NaverJavaProject/src/bit701/day0922/Ex3_MysqlGroupSawon.java
@@ -1,5 +1,11 @@
 package bit701.day0922;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Ex3_MysqlGroupSawon {
 	static final String MYSQL_DRIVER="com.mysql.cj.jdbc.Driver";
 	static final String MYSQL_URL="jdbc:mysql://localhost:3306/bit701?serverTimezone=Asia/Seoul";
@@ -18,6 +24,41 @@ public class Ex3_MysqlGroupSawon {
 		String sql="select buseo,count(*) count,max(score) maxscore,"
 				+ "min(score) minscore,avg(score) avgscore from sawon group by buseo";
 		
+		Connection conn=null;
+		Statement stmt=null;
+		ResultSet rs=null;
+		
+		try {
+			conn=DriverManager.getConnection(MYSQL_URL, "root", "1234");
+			stmt=conn.createStatement();
+			rs=stmt.executeQuery(sql);
+			
+			System.out.println("부서\t인원수\t최고점수\t최저점수\t평균");
+			System.out.println("=".repeat(30));
+			while(rs.next())
+			{
+				String buseo=rs.getString("buseo");
+				int count=rs.getInt("count");
+				int maxscore=rs.getInt("maxscore");
+				int minscore=rs.getInt("minscore");
+				double avgscore=rs.getDouble("avgscore");
+				
+				System.out.println(buseo+"\t"+count+"\t"+maxscore+"\t"+minscore+"\t"+avgscore);
+			}
+			
+		} catch (SQLException e) {
+			System.out.println("Mysql 연결 실패:"+e.getMessage());
+		}finally {
+			try {
+				rs.close();
+				stmt.close();
+				conn.close();
+			}catch (SQLException|NullPointerException e) {
+				// TODO: handle exception
+			}
+		}
+		
+		
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,3 +67,13 @@ public class Ex3_MysqlGroupSawon {
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
---
+++ javawork/NaverJavaProject/src/bit701/day0925/Ex1_OracleShopInsert.java
@@ -0,0 +1,18 @@
+package bit701.day0925;
+
+import db.DbConnect;
+
+public class Ex1_OracleShopInsert {
+	DbConnect db=new DbConnect();
+	
+	public void shopInsert()
+	{
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex1_OracleShopInsert ex=new Ex1_OracleShopInsert();
+		ex.shopInsert();
+	}
+
+}
