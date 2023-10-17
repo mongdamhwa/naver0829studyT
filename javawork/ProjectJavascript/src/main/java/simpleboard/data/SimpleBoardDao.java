f931992b83ef001bab5b31e4e315ded937d0020c
moonhee <zipsy327@gmail.com>
2023-10-17 16:56:16 +0900

    simpleboard 완성

--- javawork/ProjectJavascript/src/main/java/simpleboard/data/SimpleBoardDao.java
+++ javawork/ProjectJavascript/src/main/java/simpleboard/data/SimpleBoardDao.java
@@ -159,6 +159,44 @@ public class SimpleBoardDao {
 			db.dbClose(pstmt, conn);
 		}
 	}
+
+	//수정
+	public void updateBoard(SimpleBoardDto dto)
+	{
+		String sql="";
+		//사진은 수정하지 않았을경우 null 값을 가진다
+		//null 이 아닐경우에만 photo 를 수정한다
+		if(dto.getPhoto()==null) {
+			//사진 수정 안함
+			sql="update simpleboard set writer=?,subject=?,content=? where num=?";
+		}else {
+			//사진을 수정하는 경우
+			sql="update simpleboard set writer=?,subject=?,content=?,photo='"
+					+dto.getPhoto()+"' "
+					+ "where num=?";
+		}
+		
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+
+		try {
+			pstmt=conn.prepareStatement(sql);
+			//바인딩
+			pstmt.setString(1,dto.getWriter());
+			pstmt.setString(2,dto.getSubject());
+			pstmt.setString(3,dto.getContent());
+			pstmt.setString(4,dto.getNum());
+
+			//실행
+			pstmt.execute();
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(pstmt, conn);
+		}
+	}
 }
 
 
