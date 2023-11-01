43a83128e0f7337e84a69da4228e87a457cff161
moonhee <zipsy327@gmail.com>
2023-11-01 15:00:35 +0900

    myshop search

--- javawork/ProjectJavascript/src/main/java/myshop/data/MyShopDao.java
+++ javawork/ProjectJavascript/src/main/java/myshop/data/MyShopDao.java
@@ -11,6 +11,42 @@ import mysql.db.DbConnect;
 
 public class MyShopDao {
 	DbConnect db=new DbConnect();
+	//검색 결과 출력
+	public List<MyShopDto> getSearchSangpum(String search)
+	{
+		List<MyShopDto> list=new Vector<MyShopDto>();
+		Connection conn=db.getConnection();
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		String sql="select * from myshop where sangpum like ? order by num";
+
+		try {
+			pstmt=conn.prepareStatement(sql);
+			//바인딩
+			pstmt.setString(1, "%"+search.trim()+"%");
+			rs=pstmt.executeQuery();
+
+			while(rs.next())
+			{
+				MyShopDto dto=new MyShopDto();//반드시 while문 안에 선언
+				dto.setNum(rs.getInt("num"));
+				dto.setSangpum(rs.getString("sangpum"));
+				dto.setPrice(rs.getInt("price"));
+				dto.setPhoto(rs.getString("photo"));
+				dto.setColor(rs.getString("color"));
+				dto.setWriteday(rs.getTimestamp("writeday"));
+				//list 에 추가
+				list.add(dto);
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			db.dbClose(rs, pstmt, conn);
+		}
+
+		return list;
+	}
 
 	//전체 출력
 	public List<MyShopDto> getAllSangpums()
