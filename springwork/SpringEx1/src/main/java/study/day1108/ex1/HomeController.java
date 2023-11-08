20f5735fc016576f1464700bd0fe3c552ed9c0e5
moonhee <zipsy327@gmail.com>
2023-11-08 17:18:55 +0900

    Spring DI

--- springwork/SpringEx1/src/main/java/study/day1108/ex1/HomeController.java
+++ springwork/SpringEx1/src/main/java/study/day1108/ex1/HomeController.java
@@ -6,6 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,8 +16,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  */
 @Controller
-public class HomeController {
-	
+public class HomeController {	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
