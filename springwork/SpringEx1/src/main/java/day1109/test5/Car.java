253fa25983a3f72ff89ed0edbc58f12fa93e83dc
moonhee <zipsy327@gmail.com>
2023-11-09 10:11:06 +0900

    Tire 예제

---
+++ springwork/SpringEx1/src/main/java/day1109/test5/Car.java
@@ -0,0 +1,15 @@
+package day1109.test5;
+
+public class Car {
+	Tire tire;
+	
+	public Car(Tire tire) {
+		// TODO Auto-generated constructor stub
+		this.tire=tire;
+	}
+
+	public void myCarInfo()
+	{
+		System.out.println("내 차의 타이어는 "+tire.getTireName()+" 입니다");
+	}
+}
---
+++ springwork/SpringEx1/src/main/java/day1109/test5/TestMainEx5.java
@@ -0,0 +1,14 @@
+package day1109.test5;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestMainEx5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ctx=new ClassPathXmlApplicationContext("appcontext2.xml");
+
+	}
+
+}
